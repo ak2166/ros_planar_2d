866b54e063057465785021790b62ff839e3ef182
Anthony Kolodzinski <anthony.kolodzinski@cloudsherpas.com>
2015-04-04 19:05:27 -0400

    Added launch file + controllers.yaml. Edited CMakeLists.txt + Package.xml.

--- src/planar_2d_planning/src/project_part_one.cpp
+++ src/planar_2d_planning/src/project_part_one.cpp
@@ -2,19 +2,37 @@
 #include <ros/ros.h>
 
 //MoveIt! includes
-#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/planning_interface/planning_interface.h>
 #include <moveit/planning_scene/planning_scene.h>
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_msgs/PlanningScene.h>
 #include <moveit/move_group_interface/move_group.h>
-#
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "plan_node");
   ros::NodeHandle nh;
   ros::AsyncSpinner spinner(1);
   spinner.start();
+  moveit::planning_interface::MoveGroup group("arm_group");
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  ros::Publisher display_publisher = nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+  moveit_msgs::DisplayTrajectory display_trajectory;
+  geometry_msgs::Pose target_pose1;
+  target_pose1.position.x = 0.02;
+  target_pose1.position.y = .30;
+  target_pose1.position.z = 0.0;
+  group.setPoseTarget(target_pose1);
+  moveit::planning_interface::MoveGroup::Plan my_plan;
+  bool success = group.plan(my_plan);
+
+  ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+  /* Sleep to give Rviz time to visualize the plan. */
+  sleep(5.0);
+
   
 }
